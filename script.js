e228ec14f7a6755be69817d63d219020a96e8cc5
MD. IMRAN ISLAM <78563770+imran0x@users.noreply.github.com>
2025-06-08 09:32:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.addEventListener('scroll', () => {
-  const hero = document.querySelector('.hero');
-  hero.style.opacity = 1 - window.scrollY / 600;
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.panel').forEach((panel, i) => {
+  gsap.from(panel, {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: panel,
+      start: "top 80%",
+      toggleActions: "play none none reverse"
+    }
+  });
 });
